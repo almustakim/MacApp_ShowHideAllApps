39248f89c61c4d531cb49145be79962dc872f692
Al Mustakim <badhonmdabdullah@gmail.com>
2020-05-09 01:08:16 +0600

    showing and hiding all runni

---
+++ Show Hide All Application/Show Hide All Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Show Hide All Application
+//
+//  Created by AL Mustakim on 8/5/20.
+//  Copyright © 2020 AL Mustakim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
